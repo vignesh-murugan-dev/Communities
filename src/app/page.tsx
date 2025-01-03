58057797bdd9a6a20900fb7ec5b6c96b66e3e80d
nammahari <hariprasanth581@gmail.com>
2025-01-03 17:08:50 +0530

    Added temp umami for analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 // import events from '../data/events.json';
+import Head from 'next/head';
 import CallToAction from './calltoaction';
 import Hero from './hero';
 import NavBar from './nav';
@@ -6,6 +7,9 @@ import Events from './events';
 
 export default function Home() {
 return (
+    <Head>
+        <script async defer src="https://cloud.umami.is/script.js" data-website-id="ff99f01a-248c-4f31-87f7-fc40499f219a"></script>
+      </Head>
     <div className='bg-[#fafafa]'>
     <div className='max-w-[1120px] mx-auto font-inter'>
         <NavBar />
