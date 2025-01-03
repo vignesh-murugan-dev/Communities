e9f8439978289ebe951786af87417276ae462268
swami1302 <swamii1413@gmail.com>
2025-01-04 01:19:17 +0530

    design consistency

--- src/app/calltoaction.tsx
+++ src/app/calltoaction.tsx
@@ -8,7 +8,8 @@ const CallToAction = () => {
         <div className="flex items-center justify-between p-8">
           <div className="max-w-2xl">
             <h3 className="text-2xl text-center md:text-left md:text-3xl text-white font-normal mb-6">
-              Know a tech event? Share it to help others find and join by adding yours to the list!
+              Know a tech event? <br/>
+              Share it to help others find and join by adding yours to the list!
             </h3>
             <div className="flex justify-center md:justify-start flex-wrap gap-4">
               <button className="px-6 py-2 bg-black text-white rounded-md hover:bg-gray-800 transition-colors" onClick={() => window.open("https://github.com/FOSSUChennai/Communities/blob/main/CONTRIBUTING.md")}>
