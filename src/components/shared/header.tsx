5dedd32a9374bab860b5464d412a586d48f48293
surendar-05 <surendarrockzz7@gmail.com>
2025-01-04 22:12:19 +0530

    feat(header): add responsive styles

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -3,11 +3,11 @@ import GitHubButton from '../github-button';
 
 export default function Header() {
   return (
-    <nav className='flex w-full flex-1 items-center justify-between bg-[#fafafa] px-4 py-4 text-black md:px-8 lg:px-16'>
+    <nav className='flex w-full items-center justify-between bg-[#fafafa] px-4 py-4 text-black md:px-8 lg:px-16'>
       <Link href='/' className='text-xl font-semibold'>
         tamilnadu.tech
       </Link>
-      <div className='space-x-4'>
+      <div className='flex items-center space-x-4'>
         <GitHubButton />
       </div>
     </nav>
